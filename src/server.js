bdd4639a9bb904175498a06c7d9288bf8e4d6796
Michael Watts <hello@michaelwatts.co>
2018-07-23 14:19:59 +0100

    server script added

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer();
+
+server.listen(3000, function() {
+  console.log(
+    "Server is listening on port 3000. Ready to accept requests. You hum it, I'll play it."
+  );
+});
