d30b5dfca665e2ab4dc27cf63391627493e9ad95
zeanort <armandwork@hotmail.co.uk>
2018-07-23 14:37:05 +0100

    Node n stuff

--- src/server.js
+++ src/server.js
@@ -1,8 +1,24 @@
 const http = require("http");
 
-const server = http.createServer();
 
-server.listen(3000, function() {
+
+const message = "This is great, node is the best, like, really."
+
+
+
+
+
+const handler = (request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+  response.write(message);
+  response.end();
+}
+
+const server = http.createServer(handler);
+
+server.listen(3000, function () {
   console.log(
     "Server is listening on port 3000. Ready to accept requests. You hum it, I'll play it."
   );
