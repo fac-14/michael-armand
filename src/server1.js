793ed2c790071690984d5f11cc277d903b2e95cb
zeanort <armandwork@hotmail.co.uk>
2018-07-24 11:52:26 +0100

    step 8 changes

---
+++ src/server1.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const handler = require('./handler')
+
+const server = http.createServer(handler);
+
+server.listen(3000, function () {
+  console.log(
+    "Server is listening on port 3000. Ready to accept requests. You hum it, I'll play it."
+  );
+});
