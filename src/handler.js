0e667f1fbb1e0676fb551a54fc43c1072be5eef0
zeanort <armandwork@hotmail.co.uk>
2018-07-24 13:05:01 +0100

    made post appear

--- src/handler.js
+++ src/handler.js
@@ -3,65 +3,82 @@ const path = require("path");
 const querystring = require("querystring");
 
 function handler(request, response) {
-  console.log(request.url, request.method);
-  let url = request.url;
-  if (url === "/") {
-    //response.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile(path.join(__dirname, "..", "/public/index.html"), function(
-      error,
-      file
-    ) {
-      if (error) {
-        console.log(error);
-        return;
-      } else {
-        response.writeHead(200, {
-          "Content-Type": "text/html"
+    console.log(request.url, request.method);
+    let url = request.url;
+    if (url === "/") {
+        //response.writeHead(200, {'Content-Type': 'text/html'})
+        fs.readFile(path.join(__dirname, "..", "/public/index.html"), function (
+            error,
+            file
+        ) {
+            if (error) {
+                console.log(error);
+                return;
+            } else {
+                response.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                response.end(file);
+            }
         });
-        response.end(file);
-      }
-    });
-  } else if (url === "/create/post") {
-    console.log("blog post handling");
-    let data = "";
-    request.on("data", function(packet) {
-      data += packet;
-    });
-    request.on("end", () => {
-      let parsed = querystring.parse(data);
-      console.log(parsed);
-      response.writeHead(301, { Location: "/index.html" });
-      response.end();
-    });
-  } else {
-    let ext = url.split(".")[1];
+    } else if (url === "/create/post") {
+        console.log("blog post handling");
+        let data = "";
+        request.on("data", function (packet) {
+            data += packet;
+        });
+        request.on("end", () => {
+            let parsed = querystring.parse(data);
+            console.log(parsed);
+            response.writeHead(301, {
+                Location: "/index.html"
+            });
+            response.end();
+        });
+    } else if (url === "/posts") {
+        fs.readFile(path.join(__dirname, "..", "/src/posts.json"), function (
+            error,
+            file
+        ) {
+            if (error) {
+                console.log(error);
+                return;
+            } else {
+                response.writeHead(200, {
+                    "Content-Type": "application/json"
+                });
+                response.end(file);
+            }
+        });
+    } else {
+        let ext = url.split(".")[1];
 
-    let contentType = {
-      html: "text/html",
-      css: "text/css",
-      png: "image/png",
-      jpg: "image/jpeg",
-      jpeg: "image/jpeg",
-      gif: "image/gif",
-      svg: "image/svg+xml",
-      ico: "image/x-icon",
-      js: "application/javascript"
-    };
-    fs.readFile(path.join(__dirname, "..", "public", url), function(
-      error,
-      file
-    ) {
-      if (error) {
-        console.log(error);
-        return;
-      } else {
-        response.writeHead(200, {
-          "Content-Type": contentType[ext]
+        let contentType = {
+            html: "text/html",
+            css: "text/css",
+            png: "image/png",
+            jpg: "image/jpeg",
+            jpeg: "image/jpeg",
+            gif: "image/gif",
+            svg: "image/svg+xml",
+            ico: "image/x-icon",
+            js: "application/javascript"
+        };
+        fs.readFile(path.join(__dirname, "..", "public", url), function (
+            error,
+            file
+        ) {
+            if (error) {
+                console.log(error);
+                return;
+            } else {
+                response.writeHead(200, {
+                    "Content-Type": contentType[ext]
+                });
+                response.end(file);
+            }
         });
-        response.end(file);
-      }
-    });
-  }
+    }
 }
 
 module.exports = handler;
